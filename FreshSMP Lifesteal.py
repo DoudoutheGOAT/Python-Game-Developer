9eedc7b0218edbfe1f6f34dc9b6e6acccd06ff0d
DoudoutheGOAT <annetta271208@gmail.com>
2026-08-21 20:01:52 +0200

    Hi freshsmp sell hearts /trade 2 week cooldown

---
+++ FreshSMP Lifesteal.py
@@ -0,0 +1,21 @@
+import pgzrun
+import random
+WIDTH=750
+HEIGHT=500
+DoudoutheGOAT=Actor("netherite_noob")
+DoudoutheGOAT.x=100
+DoudoutheGOAT.y=300
+FreshLOL=Actor("freshlol")
+FreshLOL.x=100
+FreshLOL.y=100
+def draw():
+    screen.fill("light blue")
+    FreshLOL.draw()
+    DoudoutheGOAT.draw()
+def update():
+    pass
+def on_mouse_down(pos):
+    if FreshLOL.collidepoint(pos):
+        FreshLOL.x=random.randint(50,700)
+        FreshLOL.y=random.randint(50,450)
+pgzrun.go()
